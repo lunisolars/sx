2b493bf08b1c62a4fc57ba7a849fea74b6f2b393
Natron Kan <waterbeside@163.com>
2023-04-27 22:49:28 +0800

    test jd

--- __tests__/class/jd.test.ts
+++ __tests__/class/jd.test.ts
@@ -3,9 +3,18 @@ import { JD } from '../../src/class/jd'
 // import { JD as JDX } from '../../src/utils/jd'
 
 describe('test JD Class', () => {
-  it('test JD', () => {
+  it('test JD 2023-04-14 12:00:00', () => {
     const jd = JD.fromGre({ year: 2023, month: 4, day: 14, hour: 12 }, { isUTC: true })
     expect(jd.jdn).toBe(2460049)
     expect(jd.format(`YYYY-MM-DD HH:mm:ss`)).toBe('2023-04-14 12:00:00')
+  })
+
+  it('test JD 2023-04-26 12:00:00', () => {
+    const jd = JD.fromGre({ year: 2023, month: 4, day: 26, hour: 0 }, { isUTC: true })
+    expect(jd.jdn).toBe(2460060.5)
+    expect(jd.format(`YYYY-MM-DD HH:mm:ss`)).toBe('2023-04-26 00:00:00')
+    expect(new JD(jd.jdn, { isUTC: true }).format('YYYY-MM-DD HH:mm:ss')).toBe(
+      '2023-04-26 00:00:00'
+    )
   })
 })
