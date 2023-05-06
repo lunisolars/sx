abcdd23bbe7b3c6556731db9878e927d90d0746c
Natron Kan <waterbeside@163.com>
2023-05-06 23:18:48 +0800

    test: JD and SSQ

--- __tests__/class/jd.test.ts
+++ __tests__/class/jd.test.ts
@@ -17,4 +17,32 @@ describe('test JD Class', () => {
       '2023-04-26 00:00:00'
     )
   })
+
+  it('test JD parse and fromat 2023-05-06 00:00:00', () => {
+    const jd = JD.fromGre('2023-05-06 00:00:00')
+    expect(jd.format(`YYYY-MM-DD HH:mm:ss`)).toBe('2023-05-06 00:00:00')
+    expect(jd.jdn).toBe(JD.fromGre({ year: 2023, month: 5, day: 6 }).jdn)
+    expect(jd.jdn).toBe(JD.fromGre({ year: 2023, month: 5, day: 6 }).jdn)
+  })
+
+  it('test JD add', () => {
+    const jd = JD.fromGre({ year: 2023, month: 5, day: 6, hour: 0 }, { isUTC: true })
+    // const jd2 = JD.fromGre({ year: 2023, month: 5, day: 6, hour: 0 })
+    expect(jd.add(1, 'day').format(`YYYY-MM-DD HH:mm:ss`)).toBe('2023-05-07 00:00:00')
+    expect(
+      JD.fromGre({
+        year: 2023,
+        month: 6,
+        day: 6,
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millis: 0
+      }).jdn
+    ).toBe(2460101.1666666665)
+    expect(jd.add(1, 'month').format()).toBe('2023-06-06 00:00:00')
+    expect(jd.add(1, 'year').format()).toBe('2024-05-06 00:00:00')
+    expect(JD.fromGre('2023-06-06 00:00:00').jdn).toBe(2460101.1666666665)
+    // expect(new JD(2460102.1666550925).format()).toBe('2023-06-06 00:00:00')
+  })
 })
--- __tests__/class/ssq.test.ts
+++ __tests__/class/ssq.test.ts
@@ -8,17 +8,23 @@ import { J2000 } from '../../src/constants'
 describe('test SSQ Class', () => {
   it('test SSQ 2023', () => {
     const ssq = new SSQ(2023)
-    const st = ssq.getSolarTerms(1)
-    const st2 = ssq.getSolarTerms(0)
+    // const st = ssq.getSolarTerms(1)
+    // const st2 = ssq.getSolarTerms(0)
     SSQX.calcY(Math.floor(23) * 365.2422 + 180)
-    st.map((v, i) => {
-      console.log(v, i)
-      const jd = new JD(v.jdn, { isUTC: true })
-      const jd2 = new JD(st2[i].jdn)
-      const jd3 = new JD(SSQX.ZQ[i] + J2000, { isUTC: true })
-      console.log(jd.format(), jd2.format(), jd3.format())
+    // st.map((v, i) => {
+    //   console.log(v, i)
+    //   const jd = new JD(v.jdn, { isUTC: true })
+    //   const jd2 = new JD(st2[i].jdn)
+    //   const jd3 = new JD(SSQX.ZQ[i] + J2000, { isUTC: true })
+    //   console.log(jd.format(), jd2.format(), jd3.format())
+    // })
+    const yearQS = ssq.getQS()
+    const months = ssq.getMoons(0)
+    yearQS.map((v, i) => {
+      const jd = new JD(v.dayJdn, { isUTC: true })
+      const ssqJd = new JD(SSQX.HS[i] + J2000, { isUTC: true })
+      console.log('yearqs', jd.format(), ssqJd.format(), new JD(months[i].jdn).format())
+      console.log(v)
     })
-    ssq.getQS()
   })
-
 })
